dd8c3a853c133eb9655ca9a9e9cee9df70e06a26
Shrutiruchi <46797012+Shrutiruchi@users.noreply.github.com>
2019-10-19 08:03:56 +0530

    Update etf.cpp

--- Euler Totient Function/etf.cpp
+++ Euler Totient Function/etf.cpp
@@ -8,15 +8,15 @@ void cal_etf()
 {
     for(int i = 1 ;i <= MAXN ; i++ )
     	etf[i]=i;
-    for(int i=2 ; i<= MAXN ; i++ )
-        if ( etf[i] == i)
-            for(int j = 2*i ; j <= MAXN ; j += i)
-            	etf[j] -=  etf[j]/ i ;
-    
-    etf[1]=1;
-    for(int i=2 ; i<= MAXN ; i++) {	
-        if ( etf[i]==i) //if that number is prime
-    		etf[i] = i-1;
+    }
+
+    for(int i=2;i<=MAXN;i++){
+        if(etf[i]==i){
+            etf[i]=i-1;
+            for(int j=2;j*i<=MAXN;j++){
+                etf[j*i]=(1-(1/i));
+            }
+        }
     }
 }
 
