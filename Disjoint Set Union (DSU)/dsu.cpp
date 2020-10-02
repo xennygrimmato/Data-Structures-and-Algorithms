e385799626ee90ba74d1f58f0a603b10a8220c05
rakshitkadam <50885077+rakshitkadam@users.noreply.github.com>
2020-10-02 16:02:32 +0530

    Update dsu.cpp

--- Disjoint Set Union (DSU)/dsu.cpp
+++ Disjoint Set Union (DSU)/dsu.cpp
@@ -1,54 +1,18 @@
-class UF    {
-    int *id, cnt, *sz;
-public:
-	// Create an empty union find data structure with N isolated sets.
-    UF(int N)   {
-        cnt = N;
-	id = new int[N];
-	sz = new int[N];
-        for(int i=0; i<N; i++)	{
-            id[i] = i;
-	    sz[i] = 1;
-	}
-    }
-    ~UF()	{
-	delete [] id;
-	delete [] sz;
-    }
-	// Return the id of component corresponding to object p.
-    int find(int p)	{
-        int root = p;
-        while (root != id[root])
-            root = id[root];
-        while (p != root) {
-            int newp = id[p];
-            id[p] = root;
-            p = newp;
-        }
-        return root;
-    }
-	// Replace sets containing x and y with their union.
-    void merge(int x, int y)	{
-        int i = find(x);
-        int j = find(y);
-        if (i == j) return;
-		
-		// make smaller root point to larger one
-        if   (sz[i] < sz[j])	{ 
-		id[i] = j; 
-		sz[j] += sz[i]; 
-	} else	{ 
-		id[j] = i; 
-		sz[i] += sz[j]; 
-	}
-        cnt--;
-    }
-	// Are objects x and y in the same set?
-    bool connected(int x, int y)    {
-        return find(x) == find(y);
-    }
-	// Return the number of disjoint sets.
-    int count() {
-        return cnt;
-    }
-};
+
+ll p[100004];
+ll size[100005];
+ll find(ll x)
+{
+    return (p[x]==x?(x):(x=find(p[x])));
+}
+void  unite(ll x,ll y)
+{
+    ll xx = find(x);ll yy =find(y);
+    if(xx==yy)return;
+    if(size[xx]>size[yy])swap(xx,yy);
+    size[p[yy]]+=size[p[xx]];
+    size[p[xx]]=0;
+    p[xx]=p[yy];
+    
+}
+Mistakes I made -> forgot to swap if size of xx is greater, forgot to return if in unite both elements parents are already same.
