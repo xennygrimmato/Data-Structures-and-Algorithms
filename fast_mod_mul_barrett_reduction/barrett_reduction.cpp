78d248ce7da7337f2d0e468bb1235e59987033a5
Vaibhav Tulsyan <vstulsyan@gmail.com>
2020-11-16 17:18:30 +0530

    Create barrett_reduction.cpp

---
+++ fast_mod_mul_barrett_reduction/barrett_reduction.cpp
@@ -0,0 +1,41 @@
+// Fast modular multiplication by barrett reduction
+// Reference: https://en.wikipedia.org/wiki/Barrett_reduction
+// NOTE: reconsider after Ice Lake
+struct barrett {
+    unsigned int _m;
+    unsigned long long im;
+
+    // @param m `1 <= m < 2^31`
+    barrett(unsigned int m) : _m(m), im((unsigned long long)(-1) / m + 1) {}
+
+    // @return m
+    unsigned int umod() const { return _m; }
+
+    // @param a `0 <= a < m`
+    // @param b `0 <= b < m`
+    // @return `a * b % m`
+    unsigned int mul(unsigned int a, unsigned int b) const {
+        // [1] m = 1
+        // a = b = im = 0, so okay
+
+        // [2] m >= 2
+        // im = ceil(2^64 / m)
+        // -> im * m = 2^64 + r (0 <= r < m)
+        // let z = a*b = c*m + d (0 <= c, d < m)
+        // a*b * im = (c*m + d) * im = c*(im*m) + d*im = c*2^64 + c*r + d*im
+        // c*r + d*im < m * m + m * im < m * m + 2^64 + m <= 2^64 + m * (m + 1) < 2^64 * 2
+        // ((ab * im) >> 64) == c or c + 1
+        unsigned long long z = a;
+        z *= b;
+#ifdef _MSC_VER
+        unsigned long long x;
+        _umul128(z, im, &x);
+#else
+        unsigned long long x =
+            (unsigned long long)(((unsigned __int128)(z)*im) >> 64);
+#endif
+        unsigned int v = (unsigned int)(z - x * _m);
+        if (_m <= v) v += _m;
+        return v;
+    }
+};
