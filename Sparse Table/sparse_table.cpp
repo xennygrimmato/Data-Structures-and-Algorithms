e095b0deea39a9f02297a5fd0c76aaec90136b7c
Vaibhav Tulsyan <vstulsyan@gmail.com>
2021-06-27 00:02:53 +0530

    Create sparse_table.cpp

---
+++ Sparse Table/sparse_table.cpp
@@ -0,0 +1,36 @@
+// tourist's sparse table that takes a `function` as a templated param.
+// https://www.codechef.com/viewsolution/48120758
+/**
+ *    author:  tourist
+ *    created: 26.06.2021 17:09:42       
+**/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template <typename T, class F = function<T(const T&, const T&)>>
+class SparseTable {
+ public:
+  int n;
+  vector<vector<T>> mat;
+  F func;
+
+  SparseTable(const vector<T>& a, const F& f) : func(f) {
+    n = static_cast<int>(a.size());
+    int max_log = 32 - __builtin_clz(n);
+    mat.resize(max_log);
+    mat[0] = a;
+    for (int j = 1; j < max_log; j++) {
+      mat[j].resize(n - (1 << j) + 1);
+      for (int i = 0; i <= n - (1 << j); i++) {
+        mat[j][i] = func(mat[j - 1][i], mat[j - 1][i + (1 << (j - 1))]);
+      }
+    }
+  }
+
+  T get(int from, int to) const {
+    assert(0 <= from && from <= to && to <= n - 1);
+    int lg = 32 - __builtin_clz(to - from + 1) - 1;
+    return func(mat[lg][from], mat[lg][to - (1 << lg) + 1]);
+  }
+};
